799bc6049f0ca9e176ff22c54c8941ebe1ad8296
JairoPacheco <pachecocamposjairo@gmail.com>
2020-07-19 10:53:25 -0600

    dijsktra

--- src/proyectoanalisisii/Main.java
+++ src/proyectoanalisisii/Main.java
@@ -1,11 +1,24 @@
 package proyectoanalisisii;
 
+import dinamicAlgorithm.DijkstraAlgorithm;
+import java.util.LinkedList;
+import proyectoanalisisii.graph.Graph;
+import proyectoanalisisii.graph.Vertex;
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Graph grafo = new Graph();
+        grafo.createGraph(10);
+        DijkstraAlgorithm disjkstra = new DijkstraAlgorithm(grafo);
+        disjkstra.execute(grafo.firstVertex);
+        LinkedList<Vertex> path = disjkstra.getPath(grafo.lastVertex);
+        for (Vertex vertex : path) {
+            System.out.println(vertex.getNumber());
+        }
     }
 
 }
